a40b4c2b32840c681154fff818fc6c25c0b3fe8b
bschiranth <bschiranth@gmail.com>
2017-08-14 13:10:11 -0700

    Initial Commit

---
+++ PitchMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchMe
+//
+//  Created by Chiranth Bangalore Sathyaprakash on 8/14/17.
+//  Copyright © 2017 Chiranth Bangalore Sathyaprakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
